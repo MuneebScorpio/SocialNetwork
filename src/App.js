4f4584ebf8934327d4f6040bc40c0fbeba8ad252
KingScorpio <muneebwaseem40@gmail.com>
2022-08-04 16:08:37 +0500

    aaaa

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import RootNavigator from './navigators/RootNavigator';
+
+const App = () => {
+  return (
+    <View>
+      <Text>test</Text>
+    </View>
+  );
+};
+
+export default App;
